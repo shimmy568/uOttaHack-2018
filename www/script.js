c946da4ea2e1acb70a9e2d80b0e1bb39e0243d00
ElliottBarbeau <elliottbarbeau@gmail.com>
2018-02-17 18:31:51 -0500

    jQuery post request

--- www/script.js
+++ www/script.js
@@ -1,7 +1,21 @@
-<script src="script.js">
-        function show(shown, hidden) {
-          document.getElementById(shown).style.display='block';
-          document.getElementById(hidden).style.display='none';
-          return false;
-        }
-        </script>
+
+function show(shown, hidden) {
+  document.getElementById(shown).style.display='block';
+  document.getElementById(hidden).style.display='none';
+  return false;
+}
+
+function dropin(){
+  $.ajax({
+    type: "POST",
+    url: '/app/register',
+    data: {
+      name: document.getElementById('name').value,
+      healthNumber: document.getElementById('hCard').value,
+      studentNumber: document.getElementById('sCard').value
+    },
+    success: function(data){
+      
+    }
+  });
+}
